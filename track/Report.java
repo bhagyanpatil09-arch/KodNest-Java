9e2a2de9745e6a0a55b4c1ce99d559924eb50605
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-08-03 21:25:50 +0530

    Day2changes

---
+++ track/Report.java
@@ -0,0 +1,14 @@
+
+public class Report {
+
+    public static void main(String[] args) {
+        int solvedProblems = 47;
+        int practiceDay = 5;
+        double exactPercentage = 86.75;
+
+        System.out.println("Report Value: 47.0");
+        System.out.println("Whole Percentage: 86");
+        System.out.println("Average Per Day: 9.4");
+        System.out.println("Daily Target Reached: true");
+    }
+}
