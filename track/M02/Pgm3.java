8fcba81a199623aef3a044b48d19d3ed11db24bd
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-08-06 11:35:38 +0530

    Day5changes

---
+++ track/M02/Pgm3.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Pgm3 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int num = scan.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+    }
+
+    private static class Scanner {
+
+        public Scanner() {
+        }
+    }
+}
