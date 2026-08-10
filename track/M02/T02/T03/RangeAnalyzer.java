3154a842847fe1f40074db0d5295aed992fb2b73
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-08-10 21:50:30 +0530

    Day6changes

---
+++ track/M02/T02/T03/RangeAnalyzer.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class RangeAnalyzer {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the range
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        int evenSum = 0;
+        int oddCount = 0;
+
+        // Analyze every number in the range
+        for (int i = start; i <= end; i++) {
+
+            if (i % 2 == 0) {
+                evenSum = evenSum + i;
+            } else {
+                oddCount++;
+            }
+        }
+
+        // Display the results
+        System.out.println("Even sum: " + evenSum);
+        System.out.println("Odd count: " + oddCount);
+
+        scanner.close();
+    }
+}
