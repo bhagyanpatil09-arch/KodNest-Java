128256a756460e5bf76f76dbe4a250927b74819b
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-08-14 12:34:31 +0530

    Day10changes

---
+++ track/M02/StudentApplication2.java
@@ -0,0 +1,43 @@
+
+public class Student {
+
+    String name;
+    int age;
+    double height;
+
+    Student() {
+        name = "Anu";
+        age = 16;
+        height = 5.0;
+    }
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+
+    }
+}
+
+public class StudentApplication2 {
+
+    public static void main(String[] args) {
+        Student s2 = new Student();
+        s2.display();
+        Student s3 = new Student("Manu");
+        s3.display();
+        Student s1 = new Student("Raja", 18, 5.5);
+        s1.display();
+    }
+
+}
