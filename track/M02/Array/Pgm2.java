13ed8070702aafceeb3d40be18510b9488000767
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-08-17 12:03:10 +0530

    Day11changes

---
+++ track/M02/Array/Pgm2.java
@@ -0,0 +1,16 @@
+public class Pgm2 {
+
+	public static void main(String[] args) {
+		int a[] = {11, 24, 20, 43, 50};
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+		
+
+	}
+
+} 
+    
+
