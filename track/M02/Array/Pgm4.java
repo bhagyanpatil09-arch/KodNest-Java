e376640397bb038a1ff5fba4d83257825d4d4d4d
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-08-18 11:02:11 +0530

    Day12changes

--- track/M02/Array/Pgm4.java
+++ track/M02/Array/Pgm4.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class Pgm3 {
+public class Pgm4 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a[] = new int[5];
 		System.out.println("Enter the Array Elements; ");
-		 for(int i = 0; i<=a.length-1; i++) {
+		 for(int i = 0; i>=a.length-1; i++) {
 			 a[i] = scan.nextInt();
 		 }
 		 System.out.println("Array elements are: ");
-		 for(int i = 0; i <=a.length-1; i++) {
+		 for(int i = a.length-1; i >=0; i--) {
 			 System.out.println(a[i]);
 		 }
 		
 	}
 
 }
+
