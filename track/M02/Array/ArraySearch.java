021e5e5e58a33261f4e7a2b1cd66268d28c6dd28
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-08-18 12:39:06 +0530

    Add Java practice programs

--- track/M02/Array/ArraySearch.java
+++ track/M02/Array/ArraySearch.java
@@ -1,41 +1,25 @@
-import java.util.Scanner;
+class Array {
+		void search(int ref[], int key) {
+			boolean found = true;
+			for (int i= 0; i <= ref.length-1; i++) {
+				if(ref[i] == key) {
+					System.out.println("Element found at index: " +i);
+					found = false;
+					break;
+				}
+			}
+			if(!found) {
+				System.out.println("Element not found..");
+			}	
+		}
+	}
 
 public class ArraySearch {
 
-    // Method to search for an element in an integer array
-    public static int search(int[] arr, int key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == key) {
-                return i; // Return index of element if found
-            }
-        }
-        return -1; // Return -1 if key is not found in array
-    }
+	public static void main(String[] args) {
+		int arr[] = {10,20,30,40,50};
+		ArraySearch as = new ArraySearch();
+		as.search(arr, 30);
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter size of array: ");
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextInt();
-        }
-
-        System.out.print("Enter element to search: ");
-        int key = scanner.nextInt();
-
-        // Calling the search method passing int[] and int
-        int index = search(arr, key);
-
-        if (index != -1) {
-            System.out.println("Element found at index: " + index);
-        } else {
-            System.out.println("Element not found in the array.");
-        }
-
-        scanner.close();
-    }
 }
---
+++ track/M02/Array/Frequency.java
@@ -0,0 +1,30 @@
+
+public class Freq {
+	int count = 0;
+	void count(int arr[], int key) {
+		for(int i =0; i <= arr.length-1; i++) {
+			if(arr[i] == key) {
+				count++;
+			}
+		}
+		
+		System.out.println(key + ": " + count);
+	}
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+    
+}
+public class Frequency {
+
+	public static void main(String[] args) {
+		int arr[] = {10,20,10,30,10,30};
+		Freq fs = new Freq();
+		fs.count(arr,10);
+
+	}
+
+}
