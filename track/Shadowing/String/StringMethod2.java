f836da731d4d76c3826a57008a85d027042d3b6f
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-09-08 17:44:11 +0530

    updated a new module

---
+++ track/Shadowing/String/StringMethod2.java
@@ -0,0 +1,11 @@
+public class StringMethod2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "  Java   ";
+		System.out.println(s1);
+		System.out.println(s1.trim());
+
+	}
+
+}
