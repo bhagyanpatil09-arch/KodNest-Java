d5b12a5d49601fe94ae6110af8f6e9b4ba6ec71b
Bhagyashree Patil <bhagyanpatil09@gmail.com>
2026-09-09 16:53:32 +0530

    StringComparision

---
+++ track/Shadowing/String/StringComparision.java
@@ -0,0 +1,12 @@
+public class StringComparision {
+
+	public static void main(String[] args) {
+		String s1 = "Java";
+		String res = s1.toUpperCase();
+		System.out.println(res);
+		System.out.println(s1);
+
+	}
+
+}
+
--- track/Shadowing/String/StringMethod.java
+++ track/Shadowing/String/StringMethod.java
@@ -6,16 +6,21 @@ public class StringMethod {
 		System.out.println(str.toLowerCase());
 		System.out.println(str.toUpperCase());
 		System.out.println(str.charAt(3));
+
 		System.out.println(str.contains("Nest"));
 		System.out.println(str.contains("nest"));
+
 		System.out.println(str.startsWith("Kod"));
 		System.out.println(str.startsWith("Nest"));
+        
 		System.out.println(str.endsWith("ies"));
 		System.out.println(str.endsWith("kod"));
+
 		System.out.println(str.indexOf('K'));
 		System.out.println(str.indexOf('e'));
 		System.out.println(str.length());
 		System.out.println(str.replace('e', 'A'));
+
 		System.out.println(str.substring(5));
 		System.out.println(str.substring(5, 14));
 	}
